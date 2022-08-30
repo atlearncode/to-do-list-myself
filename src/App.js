9bd24499eb45a704d1cdd88957e9848caad2a2c9
Alexandr <alexandrterzi1@gmail.com>
2022-08-30 13:26:26 -0700

    NEW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {nanoid} from "nanoid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [task, setTask] = useState([{id: nanoid(), value: 'JSS'}])
+
+    const [newTask, setNewTask] = useState('')
+
+    const [updateTask, setUpdateTask]=useState('')
+
+    const addNewTask = () => {
+        task.push({id: nanoid(), value: newTask})
+        setTask(task)
+        setNewTask('')
+    }
+
+    const deleteTask = (index) => {
+        const newArr = task.filter(el => task.indexOf(el) !== index)
+        setTask(newArr)
+    }
+
+    const up = (index) => {
+        const newArr = task.slice();
+        const newObj = task[index]
+        newArr.splice(index, 1)
+        newArr.splice(index - 1, 0, newObj)
+        setTask(newArr)
+    }
+
+    const down = (index) => {
+        const newArr = task.slice();
+        const newObj = task[index]
+        newArr.splice(index, 1)
+        newArr.splice(index + 1, 0, newObj)
+        setTask(newArr)
+    }
+
+    const updateTask = () => {
+
+    }
+
+    return (
+        <div>
+            <h1>Todoist</h1>
+            <input type={"text"} value={newTask} onChange={e => setNewTask(e.target.value)}/>
+            <button onClick={() => addNewTask()}>Add</button>
+            <hr/>
+            <ol>
+                {task.map((el, index) =>
+                    <li key={el.id}>
+                        {el.value}
+                        <button onClick={() => deleteTask(index)}>Delete</button>
+                        <button onClick={()=> updateTask()}>Update</button>
+                        <button onClick={() => up(index)} disabled={index === 0}>Up</button>
+                        <button onClick={() => down(index)} disabled={index === task.length - 1}>Down</button>
+                    </li>
+                )}
+            </ol>
+        </div>
+    );
 }
 
 export default App;
